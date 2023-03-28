9a56af89e322bdee2f05e22092ab4f01578f179e
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-28 15:56:25 +0200

    + style

--- src/components/CardsItem.vue
+++ src/components/CardsItem.vue
@@ -23,15 +23,19 @@ export default {
             </div>
     
             <div>
-                Nome Carta: {{card.name}}
+                <strong>Nome Carta: </strong> 
+                <span>{{card.name}}</span>
             </div>
             <div>
-                Tipo Carta: {{card.type}}
+                <strong>Tipo Carta: </strong>
+                <span>{{card.type}}</span>
             </div>
 
             <div v-for="price in card.card_prices">
-                Prezzo su Amazon: {{ price.amazon_price }} $
+                <strong>Prezzo Amazon: </strong>
+                <span>{{price.amazon_price}} $</span>
             </div>
+
 
         </div>
 
@@ -45,12 +49,42 @@ export default {
     .card {
         display: flex;
         flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: calc(100% / 5 - 30px);
+        gap: 6px;
+        padding-bottom: 15px;
+        background-color: #51556c3f;
+
+        border-left: 2px solid white;
+        border-right: 2px solid white;
+        border-bottom: 2px solid white;
+        border-radius: 5px;
+        box-shadow: 2px 3px 8px white;
+
+        img {
+            width: 100%;
+            border-radius: 5px;
+        }
+
+        strong {
+            text-transform: uppercase;
+            font-size: 14px;
+        }
+
+        span {
+            background-color: rgba(0, 0, 0, 0.377);
+            border-radius: 10px;
+            padding: 2px 4px;
+            box-shadow: 2px 1px 2px rgba(0, 235, 235, 0.521);
+        }
+
+        hr {
+            margin-top: 15px;
+            width: 100%;
+        }
     }
 
-    img {
-        width: 300px;
-        height: auto;
-    }
 
 
 </style>
