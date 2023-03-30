10bd8455a59e50ca0ef5e73055d590f876592b70
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-30 10:22:30 +0200

    AppLoader fixed

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -21,15 +21,20 @@ export default {
 <style lang="scss" scoped>
     
     .loading {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+        position: fixed;
+        top: 0;
+        left: 0;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: 100vw;
+        height: 100vh;
 
         font-size: 50px;
         padding: 8px 50px;
-        border-radius: 30px;
-        background-color: rgba(0, 0, 255, 0.336);
+        background-color: black;
     }
 
 </style>
